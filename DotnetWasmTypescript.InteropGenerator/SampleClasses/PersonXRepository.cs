a13c6747f27666620fa6d0bfa766e618e1611473
Marc Zwart <marc@briskey.nl>
2025-11-19 23:42:09 +0100

    restore sample file

---
+++ DotnetWasmTypescript.InteropGenerator/SampleClasses/PersonXRepository.cs
@@ -0,0 +1,34 @@
+﻿using DotnetWasmTypescript.InteropGenerator.SampleClasses;
+using TypeScriptExport;
+
+namespace QR.Wasm;
+
+[TsExport]
+public partial class PersonXRepository
+{
+    public string PlaceHolderOrTsGenNoWorkey_FixThis { get; set; }
+
+
+    internal PersonX Person1 = new PersonX()
+    {
+        Name = "Alice",
+        Age = 28,
+    };
+
+    internal static PersonX Person2 = new PersonX()
+    {
+        Name = "Bob",
+        Age = 35,
+    }
+;
+
+    public PersonX GetPerson1()
+    {
+        return Person1;
+    }
+
+    public static PersonX GetPerson2()
+    {
+        return Person2;
+    }
+}
