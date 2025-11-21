e38bac04aa4d357fa2c5a839f064d6eed98af109
Marc Zwart <marc@briskey.nl>
2025-11-22 00:48:29 +0100

    clean up

--- QR.Wasm/QRHelper.cs
+++ QR.Wasm/QRHelper.cs
@@ -1,10 +1,17 @@
-﻿namespace QR.Wasm
+﻿using QRCoder;
+using System;
+
+namespace QR.Wasm
 {
     internal sealed class QRHelper
     {
         internal static string Generate(string text, int pixelsPerBlock)
         {
-            return QR.Core.Generator.Generate(text, pixelsPerBlock);
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+
+            BitmapByteQRCode qrCode = new BitmapByteQRCode(qrCodeData);
+            return Convert.ToBase64String(qrCode.GetGraphic(pixelsPerBlock));
         }
     }
 }
--- TypeShim/TsExportAttribute.cs
+++ TypeShim/TsExportAttribute.cs
@@ -3,6 +3,3 @@
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface | AttributeTargets.Enum)]
 public class TsExportAttribute : Attribute {
 }
-
-[AttributeUsage(AttributeTargets.ReturnValue | AttributeTargets.Parameter)]
-public class TsExportAsAttribute<TSGeneratorTypeHint> : Attribute { }
