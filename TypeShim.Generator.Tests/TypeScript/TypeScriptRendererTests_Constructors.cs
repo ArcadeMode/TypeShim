55a48f4510b30bd7135cb3ddb8c6f3a7c8614b50
Marc Zwart <marc.zwart@aceve.com>
2025-12-30 18:08:08 +0100

    constructor test for bad input

--- TypeShim.Generator.Tests/TypeScript/TypeScriptRendererTests_Constructors.cs
+++ TypeShim.Generator.Tests/TypeScript/TypeScriptRendererTests_Constructors.cs
@@ -206,4 +206,41 @@ export class Proxy extends ProxyBase {
     {
         Assert.Fail("Not implemented");
     }
+
+    [Test]
+    public void TypeScriptUserClassProxy_ParameterlessConstructor_AndIncompatiblePropertyType_GeneratesNoConstructor()
+    {
+        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText("""
+            using System;
+            using System.Threading.Tasks;
+            namespace N1;
+            [TSExport]
+            public class C1
+            {
+                public Version P1 { get; set; }
+            }
+        """);
+
+        SymbolExtractor symbolExtractor = new([CSharpFileInfo.Create(syntaxTree)]);
+        List<INamedTypeSymbol> exportedClasses = [.. symbolExtractor.ExtractAllExportedSymbols()];
+        Assert.That(exportedClasses, Has.Count.EqualTo(1));
+        INamedTypeSymbol classSymbol = exportedClasses[0];
+
+        InteropTypeInfoCache typeCache = new();
+        ClassInfo classInfo = new ClassInfoBuilder(classSymbol, typeCache).Build();
+
+        TypeScriptTypeMapper typeMapper = new([classInfo]);
+        TypescriptSymbolNameProvider symbolNameProvider = new(typeMapper);
+
+        RenderContext renderContext = new(classInfo, [classInfo], RenderOptions.TypeScript);
+        new TypescriptUserClassProxyRenderer(symbolNameProvider, renderContext).Render();
+
+        AssertEx.EqualOrDiff(renderContext.ToString(), """    
+export class Proxy extends ProxyBase {
+  private constructor() {}
+
+}
+
+""");
+    }
 }
