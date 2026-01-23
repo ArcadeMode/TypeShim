2cbe0277af140747eaa2f5827cdaa64b708ae736
Marc Zwart <marc@briskey.nl>
2026-01-23 11:24:57 +0100

    first error path tests, noted runtime issue, skip tests while fix pending

--- TypeShim.E2E/vitest/suite.ts
+++ TypeShim.E2E/vitest/suite.ts
@@ -1,4 +1,8 @@
 
+// Skip tests that marshall invalid values through Promise to .NET
+// currently the runtime dies when trying to marshall such values making the tests un-runnable
+// https://github.com/dotnet/runtime/pull/123523
+export const skipInvalidPromiseResolveValueTests: boolean = true;
 
 export const isBrowserMode: boolean = !!(globalThis as any).__BROWSER_MODE__;
 
