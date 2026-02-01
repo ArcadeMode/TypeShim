069e42f947eb8f7af6ef5b8e21b94f3cf2bb29d3
Marc Zwart <marc@briskey.nl>
2026-02-02 00:29:20 +0100

    add task return method class to e2e to validate compilation

---
+++ TypeShim.E2E/TypeShim.E2E.Wasm/TaskReturnMethodsClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace TypeShim.E2E.Wasm;
+
+[TSExport]
+public class TaskReturnMethodsClass
+{
+    public Task VoidTaskMethod() => Task.CompletedTask;
+    public Task<int> Int32TaskMethod() => Task.FromResult(42);
+    public Task<bool> BoolTaskMethod() => Task.FromResult(true);
+    public Task<string> StringTaskMethod() => Task.FromResult("Hello, from .NET Task");
+
+    public Task<ExportedClass> ExportedClassTaskMethod() => Task.FromResult(new ExportedClass() { Id = 420 });
+}
