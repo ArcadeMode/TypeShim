2cb7ddc2ca0980f9469dbb9b6442e015c9b0ffee
Marc Zwart <marc.zwart@aceve.com>
2026-02-04 10:38:24 +0100

    add extension tests for numericals

--- TypeShim.Generator.Tests/CSharp/JSObjectExtensionsRendererTests_Properties.cs
+++ TypeShim.Generator.Tests/CSharp/JSObjectExtensionsRendererTests_Properties.cs
@@ -9,6 +9,62 @@ namespace TypeShim.Generator.Tests.CSharp;
 
 internal class JSObjectExtensionsRendererTests_Properties
 {
+    [TestCase("short", "Int16")]
+    [TestCase("int", "Int32")]
+    [TestCase("long", "Int64")]
+    [TestCase("float", "Single")]
+    [TestCase("double", "Double")]
+    public void JSObjectExtensionsRendererTests_InstanceProperty_WithNumberType(string csTypeName, string managedSuffix)
+    {
+        string source = """
+            using System;
+            using System.Threading.Tasks;
+            namespace N1;
+            [TSExport]
+            public class C1
+            {
+                public __TYPE__ P1 { get; set; }
+            }
+        """.Replace("__TYPE__", csTypeName);
+
+        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+        SymbolExtractor symbolExtractor = new([CSharpFileInfo.Create(syntaxTree)]);
+        List<INamedTypeSymbol> exportedClasses = [.. symbolExtractor.ExtractAllExportedSymbols()];
+        Assert.That(exportedClasses, Has.Count.EqualTo(1));
+        INamedTypeSymbol classSymbol = exportedClasses.First();
+
+        InteropTypeInfoCache typeCache = new();
+        ClassInfo classInfo = new ClassInfoBuilder(classSymbol, typeCache).Build();
+
+        List<InteropTypeInfo> types = [classInfo.Properties.First().Type];
+        RenderContext extensionsRenderContext = new(classInfo, [classInfo], RenderOptions.CSharp);
+        new JSObjectExtensionsRenderer(extensionsRenderContext, types).Render();
+
+        string expected = """    
+        #nullable enable
+        // JSImports for the type marshalling process
+        using System;
+        using System.Runtime.InteropServices.JavaScript;
+        using System.Threading.Tasks;
+        public static partial class JSObjectExtensions
+        {
+            public static __TYPE__? GetPropertyAs__MANAGED__Nullable(this JSObject jsObject, string propertyName)
+            {
+                return jsObject.HasProperty(propertyName) ? MarshalAs__MANAGED__(jsObject, propertyName) : null;
+            }
+            [JSImport("unwrapProperty", "@typeshim")]
+            [return: JSMarshalAs<JSType.Number>]
+            public static partial __TYPE__ MarshalAs__MANAGED__([JSMarshalAs<JSType.Object>] JSObject obj, [JSMarshalAs<JSType.String>] string propertyName);
+        }
+        
+        """
+        .Replace("__TYPE__", csTypeName)
+        .Replace("__MANAGED__", managedSuffix);
+
+        AssertEx.EqualOrDiff(extensionsRenderContext.ToString(), expected);
+    }
+
     [Test]
     public void JSObjectExtensionsRendererTests_InstanceProperty_WithUserClassType()
     {
