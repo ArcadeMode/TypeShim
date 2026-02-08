11fdc52d009565dddb674286647c6042d761d249
Marc Zwart <marcdevin@hotmail.com>
2026-02-08 22:03:20 +0100

    WIP weaktable based function caching

---
+++ TypeShim/MarshallArgumentsTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TypeShim;
+
+public static class MarshallArgumentsTable
+{
+    private static readonly ConditionalWeakTable<Type, ConditionalWeakTable<object, object>> _typeTables = new();
+
+    public static T GetOrCreate<T>(object target, Func<T> factory) where T : class
+    {
+        ConditionalWeakTable<object, object> table = _typeTables.GetValue(typeof(T), _ => new ConditionalWeakTable<object, object>());
+        return (T)table.GetValue(target, _ => factory()!);
+    }
+}
