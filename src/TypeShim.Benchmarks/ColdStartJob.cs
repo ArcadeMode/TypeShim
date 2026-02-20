3605ca708909c9c9144c2c6f80dbf2e851a735cc
Marc Zwart <marcdevin@hotmail.com>
2026-02-20 22:50:42 +0100

    briefer test runs and remove obsolete ref

--- src/TypeShim.Benchmarks/ColdStartJob.cs
+++ src/TypeShim.Benchmarks/ColdStartJob.cs
@@ -10,8 +10,8 @@ public sealed class ColdStartConfig : ManualConfig
         Add(DefaultConfig.Instance);
         AddJob(Job.Default
             .WithStrategy(RunStrategy.ColdStart)
-            .WithWarmupCount(0) // no warm up - as users dont get this either
-            .WithIterationCount(1) // 1 run each
-            .WithLaunchCount(20)); // 20 processes
+            .WithWarmupCount(0) // no warm up - users dont get this either
+            .WithIterationCount(1)
+            .WithLaunchCount(10));
     }
 }
