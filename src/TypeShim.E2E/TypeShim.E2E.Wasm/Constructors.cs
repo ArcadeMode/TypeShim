0be12d2b99ed04047e69ddc2e785a8b652f905a2
Marc Zwart <marcdevin@hotmail.com>
2026-03-01 16:19:52 +0100

    extend e2e tests with parameterized constructors

---
+++ src/TypeShim.E2E/TypeShim.E2E.Wasm/Constructors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TypeShim.E2E.Wasm;
+
+[TSExport]
+public class IntConstructor(int i)
+{
+    public int Value { get; } = i;
+}
+
+[TSExport]
+public class StringConstructor(string s)
+{
+    public string Value { get; } = s;
+}
+
+[TSExport]
+public class MultipleConstructor(int i, string s)
+{
+    public int IntValue { get; } = i;
+    public string StringValue { get; } = s;
+}
+
+[TSExport]
+public class ExportedClassConstructor(ExportedClass e)
+{
+    public ExportedClass Value { get; } = e;
+}
+
+[TSExport]
+public class ExportedClassMultipleConstructor(ExportedClass e, ExportedClass f)
+{
+    public ExportedClass Value { get; } = e;
+    public ExportedClass Value2 { get; } = f;
+}
+
+[TSExport]
+public class ExportedClassArrayConstructor(ExportedClass[] e)
+{
+    public ExportedClass[] Value { get; } = e;
+}
+
+[TSExport]
+public class ExportedClassActionConstructor(Action<ExportedClass> e)
+{
+    public Action<ExportedClass> Value { get; } = e;
+}
