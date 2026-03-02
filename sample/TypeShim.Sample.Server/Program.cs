94047c0f992ad6e94eb045617408e5015179173e
Marc Zwart <marcdevin@hotmail.com>
2026-03-02 22:48:17 +0100

    make controllers work

--- sample/TypeShim.Sample.Server/Program.cs
+++ sample/TypeShim.Sample.Server/Program.cs
@@ -1,16 +1,17 @@
-using TypeShim.Sample;
-using TypeShim.Sample.Server.Controllers;
-
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers()
-    .AddApplicationPart(typeof(PeopleController).Assembly); ;
-var app = builder.Build();
+    .AddApplicationPart(typeof(Program).Assembly); ;
 
+WebApplication app = builder.Build();
+app.UseRouting();
 app.UseHttpsRedirection();
 app.UseAuthorization();
-//app.UseBlazorFrameworkFiles();
 app.UseWebAssemblyDebugging();
+
+#pragma warning disable ASP0014 // Suggest using top level route registrations - not compatible with usespa and spaproxy is a bit of a hassle to use
+app.UseEndpoints(ep => ep.MapControllers());
+#pragma warning restore ASP0014 // Suggest using top level route registrations
 app.UseSpa(spa =>
 {
     spa.UseProxyToSpaDevelopmentServer("http://localhost:5173/");
@@ -18,7 +19,5 @@ app.UseSpa(spa =>
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
-app.MapControllers();
 
 app.Run();
