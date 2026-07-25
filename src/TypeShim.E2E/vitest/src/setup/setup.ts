43341d20d3ff93a3c6634abf118c914b3a9ba650
Marc Zwart <marcdevin@hotmail.com>
2026-07-25 17:30:06 +0200

    update plugin, drop node resolve shim

--- src/TypeShim.E2E/vitest/src/setup/setup.ts
+++ src/TypeShim.E2E/vitest/src/setup/setup.ts
@@ -9,18 +9,7 @@ beforeAll(async () => {
 
 let runtimeInfo: any = undefined;
 async function initializeWASMRuntime(): Promise<void> {
-  let builder: any = dotnet;
-  if (!isBrowserMode) {
-    const { resolve } = await import('node:path');
-    const { pathToFileURL } = await import('node:url');
-    const wwwrootBase =
-      pathToFileURL(resolve(import.meta.dirname, '../../../TypeShim.E2E.Wasm/bin/Debug/net10.0/wwwroot')).href + '/';
-    builder = builder.withResourceLoader(
-      (_type: string, _name: string, defaultUri: string) =>
-        new URL(defaultUri.replace(/^\/+/, ''), wwwrootBase).href
-    );
-  }
-  runtimeInfo = await builder.create();
+  runtimeInfo = await dotnet.create();
   await TypeShimInitializer.initialize(runtimeInfo);
   runtimeInfo.runMain();
 }
