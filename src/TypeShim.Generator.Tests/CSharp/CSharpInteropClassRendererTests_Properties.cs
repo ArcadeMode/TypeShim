83a00970380f40b9f79113efd4aee33c999c976a
Marc Zwart <marcdevin@hotmail.com>
2026-07-30 12:43:07 +0200

    Update dependencies + dependabot.yml (#104)

--- src/TypeShim.Generator.Tests/CSharp/CSharpInteropClassRendererTests_Properties.cs
+++ src/TypeShim.Generator.Tests/CSharp/CSharpInteropClassRendererTests_Properties.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Newtonsoft.Json.Linq;
 using TypeShim.Generator.CSharp;
 using TypeShim.Generator.Parsing;
 using TypeShim.Shared;
